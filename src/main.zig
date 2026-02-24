6b15c4bddb864a3de46dea26e8fab312cd7d39e1
marcel <marcel.phippen@proton.me>
2026-02-24 22:12:00 +0100

    feat: verify grid rows and columns

--- src/main.zig
+++ src/main.zig
@@ -11,23 +11,83 @@ pub fn main(init: std.process.Init) !void {
     var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
     const stdout_writer = &stdout_file_writer.interface;
 
-    const grid: [3][3]u8 = [3][3]u8{
-        [3]u8{ 1, 1, 1 },
-        [3]u8{ 2, 2, 2 },
-        [3]u8{ 3, 3, 3 },
+    // create
+    var grid: [4][4]u8 = [4][4]u8{
+        [4]u8{ 1, 2, 3, 4 },
+        [4]u8{ 2, 3, 4, 1 },
+        [4]u8{ 3, 4, 1, 2 },
+        [4]u8{ 4, 1, 2, 3 },
     };
     try print_grid(&grid, stdout_writer);
+
+    // test
+    if (is_valid(&grid)) {
+        std.debug.print("valid!\n", .{});
+    } else {
+        std.debug.print("invalid.\n", .{});
+    }
+
+    // solve
+    // solve_grid(&grid);
+    // try print_grid(&grid, stdout_writer);
 }
 
-fn print_grid(grid: *const [3][3]u8, writer: *Io.Writer) !void {
-    try writer.print(".-.-.-.\n", .{});
-    for (grid) |line| {
-        try writer.print("|", .{});
-        for (line) |byte| {
-            try writer.print("{}|", .{byte});
+fn get_row(grid: *[4][4]u8, y: usize) ?[4]u8 {
+    if (y >= grid.len) {
+        return null;
+    }
+    return grid[y];
+}
+fn get_column(grid: *[4][4]u8, x: usize) ?[4]u8 {
+    if (x >= grid.len) {
+        return null;
+    }
+    var column: [4]u8 = [4]u8{ 0, 0, 0, 0 };
+    for (0..grid.len) |y| {
+        column[y] = grid[y][x];
+    }
+    return column;
+}
+
+fn is_valid(grid: *[4][4]u8) bool {
+    for (0..grid.len) |idx| {
+        const row = get_row(grid, idx).?;
+        const column = get_column(grid, idx).?;
+
+        for (0..grid.len) |value| {
+            if (!is_in(&row, value + 1))
+                return false;
+            if (!is_in(&column, value + 1))
+                return false;
         }
-        try writer.print("\n", .{});
-        try writer.print(".-.-.-.\n", .{});
+    }
+    return true;
+}
+
+fn is_in(array: *const [4]u8, value: usize) bool {
+    for (array) |element| {
+        if (element == value)
+            return true;
+    }
+    return false;
+}
+
+fn solve_grid(grid: *[4][4]u8) void {
+    _ = grid;
+}
+
+fn print_grid(grid: *const [4][4]u8, writer: *Io.Writer) !void {
+    try writer.print("╭" ++ "───┬" ** (grid.len - 1) ++ "───╮\n", .{});
+    for (0..grid.len) |y| {
+        try writer.print("│", .{});
+        for (0..grid[y].len) |x| {
+            try writer.print("{: ^3}│", .{grid[y][x]});
+        }
+        if (y < grid.len - 1) {
+            try writer.print("\n├" ++ "───┼" ** (grid.len - 1) ++ "───┤\n", .{});
+        } else {
+            try writer.print("\n╰" ++ "───┴" ** (grid.len - 1) ++ "───╯\n", .{});
+        }
     }
     try writer.flush();
 }
